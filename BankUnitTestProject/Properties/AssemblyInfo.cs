da076cc1cfa29bd8a53f024fa0e2b4da429bb93f
J. Keith Owens <jkowens@iu.edu>
2023-01-24 20:25:24 -0500

    Adding Unit Test To Project

---
+++ BankUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f62c9fe8-e348-4d30-9991-ae7d1a29eaf3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BankUnitTestProject/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using BankApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankUnitTestProject
+{
+    [TestClass]
+    public class BankUnitTests
+    {
+        [TestMethod]
+        public void DepositMoney_PositiveValue_ShouldReturnIncreaseBalance()
+        {
+            var bankService = new BankService();
+
+            var customerCheckingAccount = 5551111;
+
+            bankService.GetOwnerAccounts(1234);
+
+            var balanceAfterDeposit = bankService.DepositMoney(customerCheckingAccount, 500);
+
+            Assert.AreEqual(1500, balanceAfterDeposit);
+
+        }
+
+        [TestMethod]
+        public void WithdrawlMoney_LessThan500_ShouldReturnDecreaseBalance()
+        {
+            var bankService = new BankService();
+
+            var personalInvestmentAccount = 12341111;
+
+            bankService.GetOwnerAccounts(1234);
+
+            var balanceAfterWithdrawl = bankService.WithdrawlMoney(personalInvestmentAccount, 100);
+
+            Assert.AreEqual(4900, balanceAfterWithdrawl);
+
+        }
+
+
+        [TestMethod]
+        public void WithdrawlMoney_MoreThan500_ShouldNotChange()
+        {
+            var bankService = new BankService();
+
+            var personalInvestmentAccount = 12341111;
+
+            bankService.GetOwnerAccounts(1234);
+
+            var balanceAfterWithdrawl = bankService.WithdrawlMoney(personalInvestmentAccount, 600);
+
+            Assert.AreEqual(5000, balanceAfterWithdrawl);
+
+        }
+
+        [TestMethod]
+        public void TransferMoney_CheckingToInvestment_ShouldIncreaseReceivingAccountAndDecreaseSendingAccount()
+        {
+            var bankService = new BankService();
+            var personalInvestmentAccount = 12341111;
+            var customerCheckingAccount = 5551111;
+
+            bankService.GetOwnerAccounts(1234);
+
+            var checkingBalance = bankService.TransferMoney(personalInvestmentAccount, customerCheckingAccount, 100);
+
+            Assert.AreEqual(900, checkingBalance);
+
+        }
+    }
+}
